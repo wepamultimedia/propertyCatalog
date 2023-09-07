fd0d6899600e3be24112ef70f4fbf26db0409984
Carlos Puche <cpuche@wepa.es>
2023-09-07 18:07:41 +0100

    Update 2023_07_18_104449_update_table_properties_google_earth.php

--- database/migrations/2023_07_18_104449_update_table_properties_google_earth.php
+++ database/migrations/2023_07_18_104449_update_table_properties_google_earth.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('procat_properties', function (Blueprint $table){
-            $table->string('google_earth')->default('');
+            $table->string('google_earth')->nullable();
         });
     }
 
